31e55bd5a069e9c7fc924e1dfc103f8778a657c3
Jesus <xjesus77@gmail.com>
2025-04-11 21:01:45 +0200

    first commit

---
+++ JS/codigoSlideDisco.js
@@ -0,0 +1,12 @@
+var splide = new Splide( '.splide', {
+    type   : 'loop',
+    perPage: 3,
+    focus  : 'center',
+    autoplay: true,
+    interval: 2500,
+    autowidth: true,
+  } );
+  
+  splide.mount();
+
+  
